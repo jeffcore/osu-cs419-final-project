39932667387c7cb95b8b13b68104c8b44b6c412f
rittie <chuaprar@onid.orst.edu>
2015-02-04 21:31:30 -0800

    Update db_creation.sql

--- db_creation.sql
+++ db_creation.sql
@@ -20,6 +20,6 @@ CREATE TABLE  appointment (
 INSERT appointment (advisor_name, student_name,
 advisor_email, student_email, appointment_date, appointment_start_time,
 appointment_end_time)
-VALUES ('Jeff Rix', 'Rittie Chuaprasert',
-'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu',
-'2015-02-09', '11:30:00', '12:00:00');
+VALUES ("Jeff Rix", "Rittie Chuaprasert",
+"rixj@onid.oregonstate.edu", "chuaprar@onid.oregonstate.edu",
+"2015-02-09", "11:30:00", "12:00:00");
