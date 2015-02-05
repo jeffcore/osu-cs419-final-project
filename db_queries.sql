5f875f37e3a1b0692b0ab2ac9d14af4a578d8ea3
Jeffrey Rix <rixj@flip2.engr.oregonstate.edu>
2015-02-04 18:37:23 -0800

    DB Queries

---
+++ db_queries.sql
@@ -0,0 +1,7 @@
+INSERT appointment ('Jeff Rix', 'Rittie Chuaprasert', 
+'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu', 
+'2015-02-09', '11:30:00', '12:00:00') VALUES (advisor_name, student_name, 
+advisor_email, student_email, appointment_date, appointment_start_time, 
+appointment_end_time)
+
+DELETE FROM appointment WHERE id = '1'
