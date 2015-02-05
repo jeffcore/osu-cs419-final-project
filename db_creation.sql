0f4799f3b4a7ba25caa38954ff409279409ca9e2
Jeffrey Rix <rixj@flip2.engr.oregonstate.edu>
2015-02-04 20:07:04 -0800

    DB Files edits

--- db_creation.sql
+++ db_creation.sql
@@ -17,11 +17,9 @@ CREATE TABLE  appointment (
 
 
 --Prefill Data
-INSERT appointment ('Jeff Rix', 'Rittie Chuaprasert',
-'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu',
-'2015-02-09', '11:30:00', '12:00:00')
-VALUES (advisor_name, student_name,
+INSERT appointment (advisor_name, student_name,
 advisor_email, student_email, appointment_date, appointment_start_time,
-appointment_end_time);
-
-
+appointment_end_time)
+VALUES ('Jeff Rix', 'Rittie Chuaprasert',
+'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu',
+'2015-02-09', '11:30:00', '12:00:00');
--- db_queries.sql
+++ db_queries.sql
@@ -1,9 +1,9 @@
-INSERT appointment ('Jeff Rix', 'Rittie Chuaprasert', 
-'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu', 
-'2015-02-09', '11:30:00', '12:00:00') 
-VALUES (advisor_name, student_name, 
-advisor_email, student_email, appointment_date, appointment_start_time, 
-appointment_end_time);
+INSERT appointment (advisor_name, student_name,
+advisor_email, student_email, appointment_date, appointment_start_time,
+appointment_end_time)
+VALUES ('Jeff Rix', 'Rittie Chuaprasert',
+'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu',
+'2015-02-09', '11:30:00', '12:00:00');
 
 DELETE FROM appointment WHERE id = '1';
 
