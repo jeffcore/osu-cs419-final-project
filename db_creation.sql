12ee66f9aebecd4acda380e1502372b72080e57e
Jeffrey Rix <rixj@flip2.engr.oregonstate.edu>
2015-02-04 20:01:11 -0800

    DB File update

--- db_creation.sql
+++ db_creation.sql
@@ -14,3 +14,14 @@ CREATE TABLE  appointment (
 	`date_created` date,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
+
+
+--Prefill Data
+INSERT appointment ('Jeff Rix', 'Rittie Chuaprasert',
+'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu',
+'2015-02-09', '11:30:00', '12:00:00')
+VALUES (advisor_name, student_name,
+advisor_email, student_email, appointment_date, appointment_start_time,
+appointment_end_time);
+
+
--- db_queries.sql
+++ db_queries.sql
@@ -1,7 +1,13 @@
 INSERT appointment ('Jeff Rix', 'Rittie Chuaprasert', 
 'rixj@onid.oregonstate.edu', 'chuaprar@onid.oregonstate.edu', 
-'2015-02-09', '11:30:00', '12:00:00') VALUES (advisor_name, student_name, 
+'2015-02-09', '11:30:00', '12:00:00') 
+VALUES (advisor_name, student_name, 
 advisor_email, student_email, appointment_date, appointment_start_time, 
-appointment_end_time)
+appointment_end_time);
 
-DELETE FROM appointment WHERE id = '1'
+DELETE FROM appointment WHERE id = '1';
+
+SELECT id, advisor_name, student_name,advisor_email, student_email, appointment_date, 
+appointment_start_time,appointment_end_time
+FROM appointment 
+WHERE advisor_email = 'rixj@onid.oregonstate.edu';
