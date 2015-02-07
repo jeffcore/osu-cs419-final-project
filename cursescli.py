7e4b6a19baee6362e7fbf53c2f196e0fb6ce52af
jeffrix <rixj@flip3.engr.oregonstate.edu>
2015-02-07 11:07:20 -0800

    Coded the Curses cli updated

--- cursescli.py
+++ cursescli.py
@@ -1,9 +1,9 @@
 import curses
 import traceback
 import datetime
+import binascii
 import mysql.connector
 from mysql.connector import errorcode
-
 
 def create_db_connection():
     db_user = "cs419-g8"
@@ -19,7 +19,6 @@ def create_db_connection():
 
     return cnx
 
-
 def get_appointments_list(cnx, username):
     cursor = cnx.cursor()
     query = (
@@ -31,62 +30,130 @@ def get_appointments_list(cnx, username):
     data = cursor.fetchall()   
     return data
 
-
-
+def display_main_screen(cnx, stdscr, username):    
+    error = False
+    data = None
+    while True:        
+        stdscr.addstr("Welcome to Advisor Appointment CLI!\n\n")    
+        if error:                
+            stdscr.addstr("Error: no appointments found for user.\n")   
+        else:
+            stdscr.addstr("\n")
+            
+        if username == "":
+            stdscr.addstr("Type Your OSU Username: ")            
+            curses.echo()
+            username = stdscr.getstr(3,24,30)                                
+      
+        data = get_appointments_list(cnx, username)
+        rowcount = len(data)
+        
+        if rowcount == 0: 
+            error = True
+            username = ""
+            stdscr.clear()
+        else:
+            break
+    
+    return data, username
+    
+def display_appointments(stdscr, data, appt_num, appt_per_page, appt_page_number):
+    # display appointments 
+    if appt_num > 0:
+        stdscr.addstr("\nAppointment " + str(appt_num) + " Has Been Deleted\n")     
+    stdscr.addstr("ID\tStudent Name\t\tStudent Email\t\t\tAppt Date\tStart Time\tEnd Time\n")  
+    for row in data:            
+        row_text = str(row[0]) + "\t"
+        row_text += row[1] + "\t"
+        row_text += row[2] + "\t"
+        # row_text = row_text + str(datetime.datetime.strptime(row[3], f))            
+        row_text += str(row[3]) + "\t"
+        row_text += str(row[4]) + "\t"
+        row_text += str(row[5])
+        stdscr.addstr(row_text + "\n")    
+    
+    if len(data) > (appt_page_number  * appt_per_page):
+        stdscr.addstr("       See More Results - Press UP or DOWN Arrow") 
+        
+    
+def get_appointment_number(stdscr):   
+    #get action from user
+    stdscr.addstr(17,0, "\nDelete Appointment - Enter Number (or 'q' to quit)\n")   
+    input = ""
+    quit = False
+    while True:
+        event = stdscr.getch(19,0)
+        # stdscr.addstr(str(event))    
+        if event == ord("q"): 
+            quit = True
+            break
+        elif ord("0") <= event <= ord("9"):
+            input += chr(event)            
+        elif event == 10:
+            break           
+        elif event == curses.KEY_DOWN:            
+            break            
+        elif event == curses.KEY_UP:
+            break  
+        elif event == 10:
+            break           
+        else:
+            None   
+    return input, quit
+    
 def main():
     cnx = create_db_connection()
     f = '%Y-%m-%d'
-     
+    username = ""
+    appt_num = 0
+    appt_per_page = 10
+    appt_page_number = 1
     try:
         #initialize curses
         stdscr = curses.initscr()
+        scrsize = stdscr.getmaxyx()
+        linesused = 7
         #Turn off echoing of keys
         #enter cbreak mode
         curses.noecho()
         curses.cbreak()
         stdscr.keypad(1)
-        
-        # get username
-        error = False        
-        while True:        
-            stdscr.addstr("Welcome to Advisor Appointment CLI!\n\n")    
-            if error:                
-                stdscr.addstr("Error: no appointments for user.\n")   
-            else:
-                stdscr.addstr("\n")
                 
-            stdscr.addstr("Type Your OSU Username: ")            
-            curses.echo()
-            username = stdscr.getstr(3,24,30)                    
-            data = get_appointments_list(cnx, username)
-            if len(data) == 0: 
-                error = True
-                stdscr.clear()
-            else:
-                break
-        
-        # display appointments         
-        stdscr.addstr("ID\tStudent Name\t\tStudent Email\t\t\tAppt Date\tStart Time\tEnd Time\n")  
-        for row in data:            
-            row_text = str(row[0]) + "\t"
-            row_text = row_text + row[1] + "\t"
-            row_text = row_text + row[2] + "\t"
-            # row_text = row_text + str(datetime.datetime.strptime(row[3], f))            
-            row_text = row_text + str(row[3]) + "\t"
-            row_text = row_text + str(row[4]) + "\t"
-            row_text = row_text + str(row[5])
-            stdscr.addstr(row_text + "\n")   
-        
-        #get action from user
-        stdscr.addstr("\nDelete Appointment - Enter Number (or 'q' to quit)\n")   
         while True:
-            event = stdscr.getch()
-            if event == ord("q"): 
-                break
-            else:
-                stdscr.addstr("\nAppointment " + str(int(event))+ " Has Been Deleted\n")   
+            stdscr.clear()
+            # load top of screen includes username search
+            data, username = display_main_screen(cnx, stdscr, username)
+                        
+            # display appointments         
+            display_appointments(stdscr, data, appt_num, appt_per_page, appt_page_number)            
+                
+            while True:        
+                input, quit = get_appointment_number(stdscr)          
             
-
+                if not quit:
+                    stdscr.addstr(input)    
+                                
+                    try:
+                        appt_num = int(input)
+                        
+                        match = False
+                        for row in data:  
+                            if row[0] == appt_num:
+                                match = True
+                        
+                        if match:
+                            # stdscr.addstr("\nAppointment " + str(appt_num) + " Has Been Deleted\n")                             
+                            break
+                        else:
+                            stdscr.addstr("\nAppointment " + str(appt_num) + " Does Not Exist\n")    
+                    except:
+                        stdscr.addstr("Invalid number")  
+                else:
+                    break
+        
+            if quit:
+                break        
+            
         stdscr.keypad(0)
         curses.nocbreak()
         curses.echo()
@@ -99,7 +166,6 @@ def main():
         curses.nocbreak()
         curses.endwin()
         traceback.print_exc()           # Print the exception
-
 
 if  __name__ == "__main__":
     main()
